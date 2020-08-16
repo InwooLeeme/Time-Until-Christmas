41eecc0ee04a4de9c506d0a9e3e92bffc7f5f663
ItsmeLee <arsmagica90@gmail.com>
2020-08-16 17:31:51 +0900

    feat : Make christmas clock using vanilla javaScript
    Make christmas clock using vanilla javaScript

--- js-clock.js
+++ js-clock.js
@@ -2,17 +2,30 @@
 const container = document.querySelector('.container'),
     clock = document.querySelector('.clock');
 
-let christmasDay = new Date("2020.12.25");
-let nowDate = new Date();
+const dayToMilliSec = 1000 * 60 * 60 * 24,
+    hoursToMilliSec = 1000 * 60 * 60,
+    minsToMilliSec = 1000 * 60,
+    secToMilliSec = 1000;
 
-//console.log(christmasDay - nowDate);
+function getUntilTime(){
+    let christmasDay = new Date("2020.12.25");    
+    let nowDate = new Date();
+    let untilChristmas = christmasDay - nowDate;
+    
+    const    untilHours = Math.trunc((untilChristmas % (dayToMilliSec) / (hoursToMilliSec)));
+    const    untilDay = Math.trunc(untilChristmas / dayToMilliSec);
+    const    untilSec = Math.trunc((untilChristmas) % (minsToMilliSec) / secToMilliSec);
+    const    untilMins = Math.trunc((untilChristmas) % (hoursToMilliSec) / minsToMilliSec);
+
+    clock.innerText = `
+    ${untilDay}d:${untilHours}h:${untilMins}m:${untilSec}s
+    `;
+}
 
 
 function init(){
-    let untilChristmas = christmasDay - nowDate;
-    const untilDay = Math.trunc(untilChristmas / (1000 * 60 * 60 * 24));
-    const untilHours = Math.trunc((untilChristmas % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
-    
+    getUntilTime();
+    setInterval(getUntilTime,1000);
 }
 
 init();
